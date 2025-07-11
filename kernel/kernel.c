7ccca7dd43482ccf5885608da9a3e04c6b94baee
ngqt <nguyenquoctrungap47@gmail.com>
2025-07-11 20:04:50 +0700

    run kernel_main()

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -2,22 +2,24 @@
  * kernel/kernel.c
  * The correct 64-bit kernel code.
  */
+#include <stdalign.h>
 void print_char(char c, int row, int col, char color) {
-    char* vga_buffer = (char*)0xB8000;
-    vga_buffer[(row * 80 + col) * 2] = c;
-    vga_buffer[(row * 80 + col) * 2 + 1] = color;
+  char *vga_buffer = (char *)0xB8000;
+  vga_buffer[(row * 80 + col) * 2] = c;
+  vga_buffer[(row * 80 + col) * 2 + 1] = color;
 }
 
+void kernel_main() __attribute__((section(".text.entry")));
 void kernel_main() {
-    const char *str = "Hello from AronaOS 64-bit!";
-    const char color = 0x0A; // Green on black
+  const char *str = "Hello from AronaOS 64-bit!";
+  const char color = 0x0A; // Green on black
 
-    for (int i = 0; str[i] != '\0'; i++) {
-        // Print the string on the second row of the screen
-        print_char(str[i], 1, i, color);
-    }
+  for (int i = 0; str[i] != '\0'; i++) {
+    // Print the string on the second row of the screen
+    print_char(str[i], 1, i, color);
+  }
 
-    for (;;) {
-        asm ("hlt");
-    }
+  for (;;) {
+    asm("hlt");
+  }
 }
